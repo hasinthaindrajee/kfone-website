d407a6d2df01ccb45d7ca018569e4fc1493871ad
Chamin Jayasooriya <chamin.njay@gmail.com>
2022-10-14 15:17:05 +0530

    add auth screen template

--- src/pages/customer-portal/PhoneVerification.jsx
+++ src/pages/customer-portal/PhoneVerification.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import { useAuthContext } from '@asgardeo/auth-react';
-import { GiCrossedAirFlows } from 'react-icons/gi';
 import PrimaryButton from '../../components/buttons/PrimaryButton';
-import login_background from '../../assets/images/people/login_background_overlay.jpeg';
 import { initiatePhoneVerify, verifyPhone } from '../../api';
+import AuthTemplate from '../../templates/AuthTemplate';
 
 const PhoneVerification = () => {
   const history = useHistory();
@@ -39,14 +38,14 @@ const PhoneVerification = () => {
 
   useEffect(() => {
     if (!location.state || !sessionStorage.getItem('otp')) {
-      history.push('/');
+      // history.push('/');
       return;
     }
 
     if (location.state) {
       const decodedIDToken = location.state;
       if (decodedIDToken?.mobileNumberVerified || sessionStorage.getItem('verified')) {
-        history.push('/my-kfone');
+        // history.push('/my-kfone');
       }
       setUserId(decodedIDToken?.userid || decodedIDToken?.sub);
       setEmail(decodedIDToken?.email);
@@ -102,56 +101,42 @@ const PhoneVerification = () => {
   };
 
   return (
-    <>
-      <main className="grid grid-cols-1 md:grid-cols-3 font-body">
-        <div
-          className="col-span-2 hidden md:block md:min-h-screen bg-right bg-cover bg-no-repeat"
-          style={{
-            backgroundImage: `url(${login_background})`
-          }}></div>
-        <div className="flex flex-col h-screen w-full justify-center items-center">
-          <div className="text-center">
-            <h1 className="flex items-center w-full text-primary text-4xl font-title">
-              <GiCrossedAirFlows size={60} />
-            </h1>
-          </div>
-          <h3 className="text-2xl my-3">Verify Your Mobile</h3>
-          <div className="mt-4 w-[60%]">
-            <div className="py-2 flex flex-col items-start w-full">
-              <label className="mb-1 text-sm" htmlFor="otp">
-                We sent a verification code to your phone ending with&nbsp;
-                {phone.substring(phone.length - 4)}
-              </label>
-              <input
-                className="border border-secondary-100 rounded p-2 w-full focus-visible:outline-primary-50"
-                type="text"
-                name="otp"
-                id="otp"
-                value={otp}
-                onChange={(e) => setOtp(e.target.value)}
-                autoFocus
-              />
-            </div>
-          </div>
-          <div className="mt-4 w-[60%]">
-            <PrimaryButton
-              text={loading ? 'Verifying...' : 'Verify'}
-              disabled={loading}
-              styles="w-full justify-center"
-              onClick={handleVerify}
-            />
-            <p className="text-sm mt-2">
-              Didn&apos;t receive the code?&nbsp;
-              <a
-                className="cursor-pointer hover:underline text-secondary-500 hover:text-primary-400"
-                onClick={handleInitiate}>
-                Request again
-              </a>
-            </p>
-          </div>
+    <AuthTemplate>
+      <h3 className="text-2xl my-3">Verify Your Mobile</h3>
+      <div className="mt-4 w-[60%]">
+        <div className="py-2 flex flex-col items-start w-full">
+          <label className="mb-1 text-sm" htmlFor="otp">
+            We sent a verification code to your phone ending with&nbsp;
+            {phone.substring(phone.length - 4)}
+          </label>
+          <input
+            className="border border-secondary-100 rounded p-2 w-full focus-visible:outline-primary-50"
+            type="text"
+            name="otp"
+            id="otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            autoFocus
+          />
         </div>
-      </main>
-    </>
+      </div>
+      <div className="mt-4 w-[60%]">
+        <PrimaryButton
+          text={loading ? 'Verifying...' : 'Verify'}
+          disabled={loading}
+          styles="w-full justify-center"
+          onClick={handleVerify}
+        />
+        <p className="text-sm mt-2">
+          Didn&apos;t receive the code?&nbsp;
+          <a
+            className="cursor-pointer hover:underline text-secondary-500 hover:text-primary-400"
+            onClick={handleInitiate}>
+            Request again
+          </a>
+        </p>
+      </div>
+    </AuthTemplate>
   );
 };
 
