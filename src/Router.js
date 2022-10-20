cd0ae97542ef7a2162b35281974830608909c11e
Brion <info@brionmario.com>
2022-10-21 00:26:17 +0530

    Secure route is not needed

--- src/Router.js
+++ src/Router.js
@@ -4,33 +4,8 @@ import { useAuthContext } from '@asgardeo/auth-react';
 import HomePage from './pages/HomePage';
 import NotFoundErrorPage from './pages/404';
 import Discover from './pages/customer-portal/explore/Explore';
-import { SecureRouteWithRedirect } from './components';
 
 const Router = (props) => {
-  if (!useAuthContext()) {
-    return <RouterWithOutAuth {...props} />;
-  }
-
-  return <RouterWithAuth {...props} />;
-};
-
-const RouterWithAuth = (props) => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={(routeProps) => <HomePage {...props} {...routeProps} />} />
-        <SecureRouteWithRedirect
-          exact
-          path="/my-kfone"
-          render={(routeProps) => <Discover {...props} {...routeProps} />}
-        />
-        <Route path="*" component={NotFoundErrorPage} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
-
-const RouterWithOutAuth = (props) => {
   return (
     <BrowserRouter>
       <Switch>
