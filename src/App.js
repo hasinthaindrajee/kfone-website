135e0a0f9b41f3a6241f6915ab1074021a397e50
Brion <info@brionmario.com>
2022-10-20 22:40:19 +0530

    Abstract `SecureRouteWithRedirect`

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import NotFoundErrorPage from './pages/404';
 import Discover from './pages/customer-portal/explore/Explore';
+import { useAuthContext } from '@asgardeo/auth-react';
+import { SecureRouteWithRedirect } from './components';
 
 const App = () => {
+  if (!useAuthContext()) {
+    return <AppWithAuth />
+  }
+
+  return <AppWithOutAuth />;
+};
+
+const AppWithAuth = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/my-kfone" component={Discover} />
+        <Route path="*" component={NotFoundErrorPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+const AppWithOutAuth = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <SecureRouteWithRedirect exact path="/my-kfone" component={Discover} />
         <Route path="*" component={NotFoundErrorPage} />
       </Switch>
     </Router>
